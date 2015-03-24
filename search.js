98ae9a3142efa04a3e8b236a0e6b7468bf59df48
Denis Wolf <dear.mr.dw@gmail.com>
2015-03-24 23:58:03 +0100

    naive POC for random connections to the data sources
    just to prove the point

--- search.js
+++ search.js
@@ -1,8 +1,9 @@
 var request = require('request-promise');
-var dataURL = process.env.DATA_URL;
+var dataURLS = JSON.parse(process.env.DATA_URLS);
 
 module.exports = function(req, res, next) {
 	var params = req.query;
+	var dataURL = dataURLS[Math.floor(Math.random() * dataURLS.length)];
 	return request({
 			uri: dataURL,
 			method: 'GET',
